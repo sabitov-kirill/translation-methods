9e31e461894c88c77adcb0f524cc21f31d99a0b9
Sabitov Kirill <sabitovkirill@ya.ru>
2024-11-17 17:37:59 +0300

    Add visualizezr

--- visualizer/main.cpp
+++ visualizer/main.cpp
@@ -1,34 +1,80 @@
+#include <graphviz/cgraph.h>
 #include <graphviz/gvc.h>
- 
-int main(int argc, char **argv) {
- 
-  // set up a graphviz context
-  GVC_t *gvc = gvContext();
- 
-  // parse command line args - minimally argv[0] sets layout engine
-  gvParseArgs(gvc, argc, argv);
- 
-  // Create a simple digraph
-  Agraph_t *g = agopen("g", Agdirected, 0);
-  Agnode_t *n = agnode(g, "n", 1);
-  Agnode_t *m = agnode(g, "m", 1);
-  Agedge_t *e = agedge(g, n, m, 0, 1);
- 
-  // Set an attribute - in this case one that affects the visible rendering
-  agsafeset(n, "color", "red", "");
- 
-  // Compute a layout using layout engine from command line args
-  gvLayoutJobs(gvc, g);
- 
-  // Write the graph according to -T and -o options
-  gvRenderJobs(gvc, g);
- 
-  // Free layout data
-  gvFreeLayout(gvc, g);
- 
-  // Free graph structures
-  agclose(g);
- 
-  // close output file, free context, and return number of errors
-  return gvFreeContext(gvc);
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+#include <SyntaxAnalyzer.h>
+
+namespace {
+
+char GM_GRAP_NAME[] = "g";
+char GM_LABEL[] = "label";
+
+class GraphManager {
+public:
+  GraphManager() : _gvc(gvContext()), _g(agopen(GM_GRAP_NAME, Agdirected, 0)) {}
+
+  ~GraphManager() {
+    gvFreeLayout(_gvc, _g);
+    gvFreeContext(_gvc);
+    agclose(_g);
+  }
+
+  Agnode_t *addNode(const std::string &name) {
+    std::string uniqueId = std::to_string(_nodeIndex++);
+
+    Agnode_t *newNode = agnode(_g, const_cast<char *>(uniqueId.c_str()), 1);
+    agset(newNode, GM_LABEL, const_cast<char *>(name.c_str()));
+    _nodeIds[newNode] = std::move(uniqueId);
+
+    return newNode;
+  }
+
+  Agedge_t *addEdge(Agnode_t *a, Agnode_t *b) {
+    return agedge(_g, a, b, 0, 1);
+  }
+
+  void render(const std::string &fileName, const std::string &engine = "dot",
+              const std::string &format = "png") {
+    gvLayout(_gvc, _g, engine.c_str());
+    gvRenderFilename(_gvc, _g, format.c_str(), fileName.c_str());
+  }
+
+private:
+  GVC_t *_gvc;
+  Agraph_t *_g;
+  std::size_t _nodeIndex{};
+  std::unordered_map<Agnode_t *, std::string> _nodeIds;
+};
+
+void astToAg(GraphManager &g, NameASTNode &parent, Agnode_t *parentNode) {
+  for (auto &&child : parent.children) {
+    Agnode_t *childNode = g.addNode(child.data);
+    g.addEdge(parentNode, childNode);
+    astToAg(g, child, childNode);
+  }
+}
+
+void astToAg(GraphManager &g, NameASTNode &root) {
+  Agnode_t *rootNode = g.addNode(root.data);
+  astToAg(g, root, rootNode);
+}
+
+} // namespace
+
+int main(int argc, char *argv[]) {
+  if (argc != 3)  {
+    std::cerr << "Wrong usage. Use: " << argv[0] << " [expression] [output file]\n";
+    return 1;
+  }
+
+  StringSource ss(argv[1]);
+  LexicalAnalyzer<StringSource> la(std::move(ss));
+  StringSyntaxAnalyzer sa(la);
+  auto astRoot = sa.parse();
+
+  GraphManager g;
+  astToAg(g, astRoot);
+  g.render(argv[2]);
 }
